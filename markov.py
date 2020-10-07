e53dc942e920e65e584507b505861a44adbf691c
ashley kwan <ashleykwan888@gmail.com>
2020-10-07 00:46:13 +0000

    markov chains complete

--- markov.py
+++ markov.py
@@ -2,6 +2,8 @@
 
 from random import choice
 
+import sys
+
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
@@ -10,9 +12,12 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
+    contents = open(file_path).read()
 
-    return 'Contents of your file as one long string'
+    return contents
+
+print(open_and_read_file("green-eggs.txt"))
+
 
 
 def make_chains(text_string):
@@ -43,9 +48,29 @@ def make_chains(text_string):
     chains = {}
 
     # your code goes here
+    words = text_string.split()
+    words.append(None)
+    
+    #list_of_values = []
 
+    for i in range(len(words) - 2):
+
+        key = (words[i], words[i + 1])
+
+        value = words[i + 2]
+
+        if key not in chains:
+            chains[key] = []
+        # chains[i] = (words[i], words[i + 1])
+        #list_of_values.append(chains[words][i + 2])
+        #(chains[words][i], chains[words][i + 1]) 
+        # chains.get((words[i], words[i + 1]), words[i + 2])
+        chains[key].append(value)
+        
+        
     return chains
 
+print(make_chains(open_and_read_file("green-eggs.txt")))
 
 def make_text(chains):
     """Return text from chains."""
@@ -54,10 +79,22 @@ def make_text(chains):
 
     # your code goes here
 
+    random_key = choice(list(chains.keys()))
+    words = [random_key[0], random_key[1]]
+    random_value = choice(chains[random_key])
+
+    # while key exists 
+    # do this: take random_key[1] and random_value and make a new key
+    # use that key to pick a new value, add to list, lather, rinse, repeat
+    while random_value is not None:
+        random_key = (random_key[1], random_value)
+        words.append(random_value)
+        random_value = choice(chains[random_key])
+
     return ' '.join(words)
 
-
-input_path = 'green-eggs.txt'
+input_path = sys.argv[1]
+#'green-eggs.txt'
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
@@ -69,3 +106,5 @@ chains = make_chains(input_text)
 random_text = make_text(chains)
 
 print(random_text)
+
+# print(open_and_read_file(make_chains(make_text("green-eggs.txt"))))
